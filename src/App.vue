67e15a46f580c09bdc428fe275b7c5a5f7e421f4
Yunt <PeterLiangAP@Outlook.com>
2021-05-06 03:47:58 -0400

    add pickQuest

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="nav">
-    <router-link to="/damage">面板计算</router-link> |
+    <router-link to="/damage">面板计算</router-link> / 
     <router-link to="/queue">随机选怪</router-link> 
   </div>
   <router-view/>
--- src/views/Queue.vue
+++ src/views/Queue.vue
@@ -1,12 +1,24 @@
 <template>
   <div>
     <h1>随机选怪物</h1>
+    <div>
+      连续狩猎<Checkbox v-model="setting.multi" :binary="true" />
+      捕获<Checkbox v-model="setting.capture" :binary="true" />
+    </div>
     <Button label="选择" @click="pick" />
+    <Button label="选择任务" @click="pickQuest" />
     数量：<Dropdown v-model="count" :options="[1, 2, 3, 4, 5]" />
-    <Button label="移除" @click="result.shift()" />
+    <Button label="移除最上" @click="result.shift()" />
+    <Button label="清空" @click="result = []" />
     <ul>
-      <li v-for="(monster, index) in result" :key="monster.name">
-        <span @click="removeItem(index)">{{ monster.name }}</span>
+      <li v-for="(item, index) in result" :key="item.name">
+        <span @click="removeItem(index)">
+          <span v-if="item.star">
+            <b>★{{ item.star }} &nbsp;</b>{{ item.name }}
+            <span>&nbsp;/&nbsp;目标：{{ item.targets.join(", ") }} </span>
+          </span>
+          <span v-else>目标：{{ item.name }} </span>
+        </span>
       </li>
     </ul>
   </div>
@@ -22,6 +34,7 @@
 <script>
 import { ref } from "vue";
 import { info } from "../lib/quest";
+import { QuestInfo } from "../lib/questInfo";
 export default {
   name: "Queue",
   setup() {
@@ -33,8 +46,14 @@ export default {
         };
       })
     );
-    const count = ref(1);
     const result = ref([]);
+    const count = ref(1);
+    const setting = ref({
+      hr: true,
+      capture: false,
+      multi: true,
+    });
+
     const pick = () => {
       for (let i = 0; i < count.value; ++i) {
         const filtered = monsters.value.filter((item) => item.enabled);
@@ -47,7 +66,34 @@ export default {
         result.value.splice(index, 1);
       }
     };
-    return { monsters, result, removeItem, pick, count };
+
+    const pickQuest = () => {
+      for (let i = 0; i < count.value; ++i) {
+        const filtered = monsters.value.filter((item) => item.enabled);
+        const target =
+          filtered[Math.floor(Math.random() * filtered.length)].name;
+        const quests = QuestInfo.questList.filter((item) => {
+          // 选择HR，移除非HR
+          if (setting.value.hr && item.star < QuestInfo.HR) return false;
+          // 选择非捕获，移除捕获
+          if (
+            !setting.value.capture &&
+            item.type === QuestInfo.QuestType.CAPTURE
+          )
+            return false;
+          // 选择非大连续，移除大连续
+          if (!setting.value.multi && item.type === QuestInfo.QuestType.MULTI)
+            return false;
+          return item.targets.indexOf(target) !== -1;
+        });
+        const index = Math.floor(Math.random() * quests.length);
+        console.log(`${target}: ${index}`);
+        console.log(quests);
+        result.value.push(quests[index]);
+      }
+      console.log(result.value);
+    };
+    return { monsters, result, removeItem, pick, count, pickQuest, setting };
   },
 };
 </script>

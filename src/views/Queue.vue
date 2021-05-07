144e32007dbca0c047e98b0ec1b5fc1bd51c4b7a
Yunt <PeterLiangAP@Outlook.com>
2021-05-07 00:10:59 -0400

    updatecooldown

--- src/views/Queue.vue
+++ src/views/Queue.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
     <h1>随机选怪物</h1>
+    <p>选择任务每个怪物有5次的内置CD</p>
     <div>
-      连续狩猎<Checkbox v-model="setting.multi" :binary="true" />
-      捕获<Checkbox v-model="setting.capture" :binary="true" />
+      连续狩猎<Checkbox v-model="setting.multi" :binary="true" /> 捕获<Checkbox
+        v-model="setting.capture"
+        :binary="true"
+      />
     </div>
-    <Button label="选择" @click="pick" />
     <Button label="选择任务" @click="pickQuest" />
     数量：<Dropdown v-model="count" :options="[1, 2, 3, 4, 5]" />
     <Button label="移除最上" @click="result.shift()" />
@@ -23,6 +25,7 @@
     </ul>
   </div>
   <div>
+    选中了{{ selectedCount }}只怪物
     <ul>
       <li v-for="monster in monsters" :key="monster.name">
         <Checkbox v-model="monster.enabled" :binary="true" />
@@ -32,7 +35,7 @@
   </div>
 </template>
 <script>
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import { info } from "../lib/quest";
 import { QuestInfo } from "../lib/questInfo";
 export default {
@@ -43,6 +46,7 @@ export default {
         return {
           name: item,
           enabled: info.starter.indexOf(item) >= 0,
+          roundLeft: 0,
         };
       })
     );
@@ -52,6 +56,12 @@ export default {
       hr: true,
       capture: false,
       multi: true,
+    });
+    const cooldown = ref(5);
+    const selectedCount = computed(() => {
+      return monsters.value.reduce((acc, current) => {
+        return (acc += current.enabled ? 1 : 0);
+      }, 0);
     });
 
     const pick = () => {
@@ -69,9 +79,19 @@ export default {
 
     const pickQuest = () => {
       for (let i = 0; i < count.value; ++i) {
-        const filtered = monsters.value.filter((item) => item.enabled);
+        const filtered = monsters.value.filter(
+          (item) =>
+            item.enabled &&
+            (item.roundLeft === 0 || selectedCount.value <= cooldown.value)
+        );
         const target =
           filtered[Math.floor(Math.random() * filtered.length)].name;
+        monsters.value.find((item) => item.name === target).roundLeft =
+          cooldown.value;
+        monsters.value = monsters.value.map((item) => {
+          if (item.roundLeft > 0) item.roundLeft -= 1;
+          return item;
+        });
         const quests = QuestInfo.questList.filter((item) => {
           // 选择HR，移除非HR
           if (setting.value.hr && item.star < QuestInfo.HR) return false;
@@ -87,13 +107,16 @@ export default {
           return item.targets.indexOf(target) !== -1;
         });
         const index = Math.floor(Math.random() * quests.length);
-        console.log(`${target}: ${index}`);
-        console.log(quests);
         result.value.push(quests[index]);
+        console.log(monsters.value);
       }
       console.log(result.value);
     };
-    return { monsters, result, removeItem, pick, count, pickQuest, setting };
+    return {
+      ...{ monsters, result },
+      ...{ removeItem, pick, pickQuest },
+      ...{ count, setting, selectedCount, cooldown },
+    };
   },
 };
 </script>
